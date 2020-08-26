ecaa1ba2f470f1faca8da032d82e7dfb85803229
Leejaeheon <ggapdol2@gmail.com>
2020-08-26 16:55:39 +0900

    junit test

---
+++ BackEnd/footBallTogether/src/test/java/com/footBallTogether/www/footBallTogetherTest_LJH.java
@@ -0,0 +1,20 @@
+package com.footBallTogether.www;
+
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+public class footBallTogetherTest_LJH {
+	
+	@Test
+	public void unit() throws Exception{
+		Reader r = Resources.getResourceAsReader("config/SqlMapConfig.xml");
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(r);
+		SqlSession session = factory.openSession();
+		
+	}
+}
